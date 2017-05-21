3215289ca092aaf803264bca3aecaa8a654a2ccc
Dominik Kotecki <dominikus1910@gmail.com>
2017-05-21 14:14:27 +0200

    State

--- src/SentimentFS.NaiveBayes/Dto.fs
+++ src/SentimentFS.NaiveBayes/Dto.fs
@@ -1,10 +1,12 @@
 namespace SentimentFS.NaiveBayes.Dto
 
+type TrainingQuery<'a, 'b when 'a : comparison and 'b : comparison> = { value: 'a; category: 'b; weight : int option }
+
 type Category<'a when 'a: comparison> = { trainings: int; tokens: Map<'a, int> }
 
-type State<'a, 'b when 'a : comparison and 'b : comparison> = { categories: Map<'a, Category<'b>>; trainings: int; tokens: Map<'b, int> }
+type State<'a, 'b when 'a : comparison and 'b : comparison> = { categories: Map<'a, Category<'b>> }
 
 module State = 
     
     [<CompiledName("Empty")>]
-    let empty = { categories = Map.empty<'a, Category<'b>>; tokens = Map.empty<'b, int>; trainings = 0 }
+    let empty = { categories = Map.empty<'a, Category<'b>> }
