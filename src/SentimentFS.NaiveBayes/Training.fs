b8dd9ca1364283b3334d38c447c606bd1a4eb68c
Dominik Kotecki <dominikus1910@gmail.com>
2017-05-22 19:21:42 +0200

    add test for empty trainer

--- src/SentimentFS.NaiveBayes/Training.fs
+++ src/SentimentFS.NaiveBayes/Training.fs
@@ -4,7 +4,15 @@ module Trainer =
     open SentimentFS.NaiveBayes.Dto
     open SentimentFS.Core.Caching
 
+    [<Literal>]
+    let internal StateKey = "SentimentFS.NaiveBayes.Training.Trainer.StateKey"
+
     let empty<'a, 'b when 'a : comparison and 'b : comparison>() = Cache.empty<State<'a,'b>>()
 
     let train(query: TrainingQuery<_,_>)(cache: Cache<State<_, _>>) = 
         cache
+
+    let get(cache: Cache<State<_, _>>) =
+        cache |> Cache.get(StateKey)
+
+    
--- tests/SentimentFS.NaiveBayes.Tests/Training.Tests.fs
+++ tests/SentimentFS.NaiveBayes.Tests/Training.Tests.fs
@@ -1,16 +1,18 @@
 namespace SentimentFS.NaiveBayes.Tests
 
 open Expecto
+open SentimentFS.Core.Caching
+open SentimentFS.NaiveBayes.Training
 open SentimentFS.NaiveBayes.Dto
 
 module Trainer =
 
     [<Tests>]
     let tests = 
-        testList "State" [
+        testList "Trainer" [
             testList "empty" [
-                testCase "test get empty state function" <| fun _ ->
-                    let subject: State<int, string>  = State.empty()
-                    Expect.equal subject ({ categories = Map.empty<int, Category<string>> }) "should equal"
+                testCase "test get empty trainer function" <| fun _ ->
+                    let subject = Trainer.empty<int, string>() |> Trainer.get
+                    Expect.isNone subject "should be None"
             ]
         ] 
