b6c961c5ab45fec38231c99b2d4205aaba87378e
Dominik Kotecki <dominikus1910@gmail.com>
2017-05-22 18:52:24 +0200

    add SentimentFS.Core

--- src/SentimentFS.NaiveBayes/Classification.fs
+++ src/SentimentFS.NaiveBayes/Classification.fs
@@ -2,5 +2,5 @@ namespace SentimentFS.NaiveBayes.Classification
 
 module Classifier =
     open SentimentFS.NaiveBayes.Dto
-    open SentimentFS.NaiveBayes.Training.StateCache
-    let classify(element: _)(cache: Cache<_,_>) = { score = Map.empty<_, double> }
+    open SentimentFS.Core.Caching
+    let classify(element: _)(cache: Cache<State<_,_>>) = { score = Map.empty<_, double> }
--- src/SentimentFS.NaiveBayes/Training.fs
+++ src/SentimentFS.NaiveBayes/Training.fs
@@ -1,39 +1,10 @@
 namespace SentimentFS.NaiveBayes.Training
 
-module StateCache =
-    open SentimentFS.NaiveBayes.Dto
-    
-    type Msg<'TValue> =
-    | Add of string * 'TValue
-    | Get of string * AsyncReplyChannel<option<'TValue>>
-    | Clear
-
-    type Cache<'a, 'b when 'a : comparison and 'b : comparison>  = MailboxProcessor<Msg<State<'a, 'b>>>
-
-    let caching<'a, 'b when 'a : comparison and 'b : comparison>(): Cache<'a, 'b> = MailboxProcessor.Start(fun agent ->
-        let rec loop(map : Map<string, 'TValue>) =
-            async {
-                let! msg = agent.Receive()
-                match msg with
-                | Add(key, value) -> 
-                    return! loop(map.Add(key, value))
-                | Get(key, repl) ->
-                    match map.TryFind(key) with
-                    | Some(value) -> 
-                        repl.Reply(Some value) 
-                        return! loop(map)
-                    | None ->
-                        repl.Reply(None) 
-                        return! loop(map)
-                | Clear -> 
-                    return! loop(Map.empty<string, 'TValue>)}
-        loop Map.empty<string, 'TValue> )
-
 module Trainer =
     open SentimentFS.NaiveBayes.Dto
-    open StateCache
+    open SentimentFS.Core.Caching
 
-    let empty<'a, 'b when 'a : comparison and 'b : comparison>() = caching<'a, 'b>()
+    let empty<'a, 'b when 'a : comparison and 'b : comparison>() = Cache.empty<State<'a,'b>>()
 
-    let train(query: TrainingQuery<_,_>)(cache: Cache<_,_>) = 
+    let train(query: TrainingQuery<_,_>)(cache: Cache<State<_, _>>) = 
         cache
