9e5c8aa502a055263002b482956dc286b2a0e932
Dominik Kotecki <dominikus1910@gmail.com>
2017-05-24 21:23:35 +0200

    add next tests

--- tests/SentimentFS.NaiveBayes.Tests/Training.Tests.fs
+++ tests/SentimentFS.NaiveBayes.Tests/Training.Tests.fs
@@ -22,5 +22,28 @@ module Trainer =
                     let categoryOpt = subject.Value.categories.TryFind(2)
                     Expect.isSome categoryOpt "category of 2 should be some"
                     Expect.equal (categoryOpt.Value) ({ trainings = 1; tokens = ([("test", 1)] |> Map.ofList) }) "should"
+                testCase "test train function (two training, one category)" <| fun _ -> 
+                    let subject = Trainer.empty<int, string>(None) 
+                                    |> Trainer.train({ value = "test"; category = 2; weight = None }) 
+                                    |> Trainer.train({ value = "test2"; category = 2; weight = None })
+                                    |> Trainer.get
+                    Expect.isSome subject "should be some"
+                    let categoryOpt = subject.Value.categories.TryFind(2)
+                    Expect.isSome categoryOpt "category of 2 should be some"
+                    Expect.equal (categoryOpt.Value) ({ trainings = 2; tokens = ([("test", 1); ("test2", 1)] |> Map.ofList) }) "should"
+                testCase "test train function (two training, two category)" <| fun _ -> 
+                    let subject = Trainer.empty<int, string>(None) 
+                                    |> Trainer.train({ value = "test"; category = 2; weight = None }) 
+                                    |> Trainer.train({ value = "test2"; category = 1; weight = None })
+                                    |> Trainer.get
+                    Expect.isSome subject "should be some"
+                    
+                    let categoryOpt = subject.Value.categories.TryFind(2)
+                    Expect.isSome categoryOpt "category of 2 should be some"
+                    Expect.equal (categoryOpt.Value) ({ trainings = 1; tokens = ([("test", 1)] |> Map.ofList) }) "should"
+
+                    let categoryOpt2 = subject.Value.categories.TryFind(1)
+                    Expect.isSome categoryOpt2 "category of 2 should be some"
+                    Expect.equal (categoryOpt2.Value) ({ trainings = 1; tokens = ([("test2", 1)] |> Map.ofList) }) "should"
             ]
         ] 
