8708d61792a7fe097ebe2c68ffa77f4e3035fea3
Dominik Kotecki <dominikus1910@gmail.com>
2017-05-25 20:23:26 +0200

    add test for incrementTrainings function

--- tests/SentimentFS.NaiveBayes.Tests/Dto.Tests.fs
+++ tests/SentimentFS.NaiveBayes.Tests/Dto.Tests.fs
@@ -13,4 +13,10 @@ module State =
                     let subject: State<int>  = State.empty()
                     Expect.equal subject ({ categories = Map.empty<int, Category>; trainings = 0 }) "should equal"
             ]
+
+            testList "incrementTrainings" [
+                testCase "test incrementTrainings in empty state function" <| fun _ ->
+                    let subject: State<int>  = State.empty() |> State.incrementTrainings
+                    Expect.equal subject ({ categories = Map.empty<int, Category>; trainings = 1 }) "should equal"
+            ]
         ] 
