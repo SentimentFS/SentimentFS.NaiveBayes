f2037ba8481a7710c24a3d3621be05e1881d11e1
Dominik Kotecki <dominikus1910@gmail.com>
2017-05-28 09:36:00 +0200

    incrementTrainings

--- src/SentimentFS.NaiveBayes/Training.fs
+++ src/SentimentFS.NaiveBayes/Training.fs
@@ -9,25 +9,29 @@ module Trainer =
     [<Literal>]
     let internal StateKey = "SentimentFS.NaiveBayes.Training.Trainer.StateKey"
 
-    let init<'a when 'a : comparison>(config: Config option) = 
-        (Cache.empty<State<'a>>(), match config with Some c -> c | None -> Config.Default()) 
+    let init<'a when 'a : comparison>(config: Config option) =
+        (Cache.empty<State<'a>>(), match config with Some c -> c | None -> Config.Default())
 
     let parseTokens(config: Config)(word: string) =
-        let result = word 
-                        |> Tokenizer.tokenize 
-                        |> Filter.filterOut(config.stopWords) 
-                        |> List.map(config.stem) 
+        let result = word
+                        |> Tokenizer.tokenize
+                        |> Filter.filterOut(config.stopWords)
+                        |> List.map(config.stem)
         result
+
+    let incrementTrainings(state: State<_>) =
+        { state with trainings = state.trainings + 1  }
+
     let train(query: TrainingQuery<_>)(cache: Cache<State<_>>, config: Config) =
-        let state = 
+        let state =
            match cache |> Cache.get(StateKey) with
            | Some oldState -> oldState
            | None -> State.empty()
         let tokens = (query.value) |> parseTokens(config)
-        (cache, config)
-         
+        let newState = state |> incrementTrainings
+        (cache |> Cache.set(StateKey, newState), config)
+
 
     let get(cache: Cache<State<_>>, _: Config) =
         cache |> Cache.get(StateKey)
 
-    
--- tests/SentimentFS.NaiveBayes.Tests/Training.Tests.fs
+++ tests/SentimentFS.NaiveBayes.Tests/Training.Tests.fs
@@ -5,10 +5,10 @@ open SentimentFS.Core.Caching
 open SentimentFS.NaiveBayes.Training
 open SentimentFS.NaiveBayes.Dto
 
-module Trainer =       
+module Trainer =
 
     [<Tests>]
-    let tests = 
+    let tests =
         testList "Trainer" [
             testList "empty" [
                 testCase "test get empty trainer function" <| fun _ ->
@@ -16,41 +16,47 @@ module Trainer =
                     Expect.isNone subject "should be None"
             ]
             testList "parseTokens" [
-               testCase "parse" <| fun _ -> 
+               testCase "parse" <| fun _ ->
                   let tokens = "cute dog" |> Trainer.parseTokens(Config.Default())
                   Expect.equal (tokens) (["cute"; "dog"]) "tokens should has two keys"
             ]
+            testList "incrementTrainings" [
+                test "increment training" {
+                    let subject = State.empty() |> Trainer.incrementTrainings
+                    Expect.equal (subject.trainings) 1 "should equal 1"
+                }
+            ]
             testList "train" [
-                testCase "test train function (one training)" <| fun _ -> 
-                    let subject = Trainer.init<int>(None) 
-                                    |> Trainer.train({ value = "test"; category = 2; weight = None }) 
-                                    |> Trainer.get                  
+                testCase "test train function (one training)" <| fun _ ->
+                    let subject = Trainer.init<int>(None)
+                                    |> Trainer.train({ value = "test"; category = 2; weight = None })
+                                    |> Trainer.get
                     Expect.isSome subject "should be some"
-            
+
                     Expect.equal (subject.Value.trainings) (1) "trainings quantity should equal 1"
 
                     let categoryOpt = subject.Value.categories.TryFind(2)
                     Expect.isSome categoryOpt "category of 2 should be some"
                     Expect.equal (categoryOpt.Value) ({ trainings = 1; tokens = ([("test", 1)] |> Map.ofList) }) "should"
-                testCase "test train function (two training, one category)" <| fun _ -> 
-                    let subject = Trainer.init<int>(None) 
-                                    |> Trainer.train({ value = "test"; category = 2; weight = None }) 
+                testCase "test train function (two training, one category)" <| fun _ ->
+                    let subject = Trainer.init<int>(None)
+                                    |> Trainer.train({ value = "test"; category = 2; weight = None })
                                     |> Trainer.train({ value = "test2"; category = 2; weight = None })
                                     |> Trainer.get
                     Expect.isSome subject "should be some"
 
                     Expect.equal (subject.Value.trainings) (2) "trainings quantity should equal 2"
-                    
+
                     let categoryOpt = subject.Value.categories.TryFind(2)
                     Expect.isSome categoryOpt "category of 2 should be some"
                     Expect.equal (categoryOpt.Value) ({ trainings = 2; tokens = ([("test", 1); ("test2", 1)] |> Map.ofList) }) "should"
-                testCase "test train function (two training, two category)" <| fun _ -> 
-                    let subject = Trainer.init<int>(None) 
-                                    |> Trainer.train({ value = "test"; category = 2; weight = None }) 
+                testCase "test train function (two training, two category)" <| fun _ ->
+                    let subject = Trainer.init<int>(None)
+                                    |> Trainer.train({ value = "test"; category = 2; weight = None })
                                     |> Trainer.train({ value = "test2"; category = 1; weight = None })
                                     |> Trainer.get
                     Expect.isSome subject "should be some"
-                    
+
                     Expect.equal (subject.Value.trainings) (2) "trainings quantity should equal 2"
 
                     let categoryOpt = subject.Value.categories.TryFind(2)
@@ -61,4 +67,4 @@ module Trainer =
                     Expect.isSome categoryOpt2 "category of 2 should be some"
                     Expect.equal (categoryOpt2.Value) ({ trainings = 1; tokens = ([("test2", 1)] |> Map.ofList) }) "should"
             ]
-        ] 
+        ]
