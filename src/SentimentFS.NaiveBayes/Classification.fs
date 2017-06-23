a67753194d69cf875cd586e7f20336bfe0548177
Dominik Kotecki <dominikus1910@gmail.com>
2017-06-23 22:02:41 +0200

    small change

--- src/SentimentFS.NaiveBayes/Classification.fs
+++ src/SentimentFS.NaiveBayes/Classification.fs
@@ -1,5 +1,10 @@
 namespace SentimentFS.NaiveBayes.Classification
 
+module NaiveProbability =
+    let internal apriori = 2
+    let internal aposterori = 2
+    let compute = 2
+
 module Classifier =
     open SentimentFS.NaiveBayes.Dto
     open SentimentFS.Core.Caching
