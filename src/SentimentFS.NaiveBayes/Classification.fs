754ea8b67b0818cec5236a9b76aa356d16b71c4c
Dominik Kotecki <dominikus1910@gmail.com>
2017-06-28 20:35:04 +0200

    add apriori

--- src/SentimentFS.NaiveBayes/Classification.fs
+++ src/SentimentFS.NaiveBayes/Classification.fs
@@ -1,9 +1,13 @@
 namespace SentimentFS.NaiveBayes.Classification
 
 module NaiveProbability =
-    let internal apriori = 2
-    let internal aposterori = 2
-    let compute = 2
+    open SentimentFS.NaiveBayes.Dto
+    let internal apriori (state: State<_>) =
+        let l = state.categories |> Map.toList
+        let allTokensQuantity = l |> List.sumBy (fun (k,v) -> ( v.tokens |> Map.toList |> List.sumBy (fun (_, v1) -> v1 |> float)))
+        l |> List.map(fun (k,v) -> (k, ((v.tokens |> Map.toList |> List.sumBy (fun (_, v1) -> v1)) |> float) / allTokensQuantity))
+    let internal aposterori state = 2
+    let compute  state = 2
 
 module Classifier =
     open SentimentFS.NaiveBayes.Dto
